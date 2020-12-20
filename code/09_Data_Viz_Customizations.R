7462a77484ffa99f10f2aa3681c59ffa5357a838
Thiago Carvalho <thiago.bernardes.carvalho@gmail.com>
2020-12-19 20:28:23 -0800

    more ggplot customizations

--- code/09_Data_Viz_Customizations.R
+++ code/09_Data_Viz_Customizations.R
@@ -82,12 +82,65 @@ ggplot(data = poke) +
   xlab('Pokemon Main Type')+
   ylim(0,450)
 
+# change limits and labels with "scale"
+ggplot(data = poke) +
+  geom_bar(mapping = aes(x=tp, fill=tp2))+
+  theme(plot.background = element_blank(),
+        panel.background = element_blank())+
+  scale_x_discrete(name='Pokemon Main Type')+
+  scale_y_continuous(name="Number of Pokemon", 
+                     limits = c(0,450))
+
+# change colors
+ggplot(data = poke) +
+  geom_bar(mapping = aes(x=tp, fill=tp2))+
+  theme(plot.background = element_blank(),
+        panel.background = element_blank())+
+  scale_x_discrete(name='Pokemon Main Type')+
+  scale_y_continuous(name="Number of Pokemon", 
+                     limits = c(0,450))+
+  scale_fill_manual(values = c("#D94040", 
+                               "#1F764B", 
+                               "#B7B7B7",
+                               "#B883BF", 
+                               "#5278B9"),
+                    na.value="#4B524E")
 
 
-# plot.backgroud()
-# panel.backgroud()
-# panel.grid.major()
-# panel.grid.minor()
+# define list of colors
+my_colors <- c("#D94040", "#1F764B", 
+               "#B7B7B7", "#B883BF", 
+               "#5278B9") 
+
+# customizing the legend
+ggplot(data = poke) +
+  geom_bar(mapping = aes(x=tp, fill=tp2))+
+  theme(plot.background = element_blank(),
+        panel.background = element_blank())+
+  scale_x_discrete(name='Pokemon Main Type')+
+  scale_y_continuous(name="Number of Pokemon", 
+                     limits = c(0,450))+
+  scale_fill_manual(values = my_colors,
+                    na.value="#4B524E",
+                    guide=guide_legend(title = 'Second Type',
+                                       nrow=1,
+                                       label.position = 'top',
+                                       keywidth = 3))+
+  theme(legend.position = 'top')
+
+# Annotation
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  annotate('text', label='Shuckle', x=20, y=230)+
+  annotate('text', label='Deoxys', x=174, y=20)
+
+# draw lines
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  annotate('text', label='Shuckle', x=20, y=230)+
+  annotate('text', label='Deoxys', x=174, y=20)+
+  geom_hline(yintercept = mean(poke$Defense))+
+  geom_vline(xintercept = mean(poke$Attack))
 
 
 # Clear environment
