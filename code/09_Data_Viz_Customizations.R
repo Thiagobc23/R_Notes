8a452a333337f5d5a763a23dc385daae3fa40692
Thiago Carvalho <thiago.bernardes.carvalho@gmail.com>
2020-12-20 17:42:29 -0800

    custom and pre built themes

--- code/09_Data_Viz_Customizations.R
+++ code/09_Data_Viz_Customizations.R
@@ -143,30 +143,100 @@ ggplot(data = poke) +
   geom_hline(yintercept = mean(poke$Defense))+
   geom_vline(xintercept = mean(poke$Attack))
 
+# custom themes (pre built themes)
+# bw -> black and white
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_bw()
 
-# another example
+# custom themes (pre built themes)
+# minimal -> remove most elements
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_minimal()
+
+# custom themes (pre built themes)
+# void -> remove almost every element
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_void()
+
+# custom themes (pre built themes)
+# dark -> dark grey bg
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_dark()
+
+# ggthemes
+install.packages('ggthemes')
+library(ggthemes)
+
+# custom themes (ggthemes)
+# solarized -> sun/ old paper like bg
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_solarized()
+
+# custom themes (ggthemes)
+# excel -> looks like ms excel
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_excel()
+
+# custom themes (ggthemes)
+# wsj -> wall street journal
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_wsj()
+
+# custom themes (ggthemes)
+# economist -> similar to The Economist charts
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_economist()
+
+# custom themes (ggthemes)
+# fivethirtyeight -> similar to fivethirtyeight charts
+ggplot(data = poke) +
+  geom_point(mapping = aes(x=Attack, y=Defense))+
+  theme_fivethirtyeight()
+
+# Example Custom Viz
 ggplot(data = poke) +
   geom_point(mapping = aes(x=Attack, y=Defense, color=Legendary))+
-  annotate('text', label='Shuckle', x=20, y=230)+
-  annotate('text', label='Deoxys', x=174, y=20) +
-  scale_color_manual(values = c('#2ACC74', '#E9652D'),
+  annotate('text', label='Shuckle', x=20, y=230, color='white')+
+  annotate('text', label='Deoxys', x=174, y=20, color='white') +
+  scale_color_manual(values = c('#F85C54','#2ACC74'),
                     guide=guide_legend(title = 'Legendary',
                                        nrow=1,
                                        reverse=TRUE),
                     labels = c('No', 'Yes'))+
-  theme(legend.position = 'top', 
-        legend.key = element_rect(colour = NA, fill = NA),
-        plot.background = element_blank(),
-        panel.background = element_blank(),
-        panel.grid.major = element_line(color='#D9D9D9'))+
   geom_hline(yintercept = mean(poke$Defense), 
-             color='#2593D3',
-             size=1)+
+             color='#F85C54',
+             size=1, alpha=0.5)+
   geom_vline(xintercept = mean(poke$Attack), 
-             color='#2593D3',
-             size=1)+
+             color='#F85C54',
+             size=1, alpha=0.5)+
   labs(title="Pokemon Attack X Defense",
-       subtitle = "Stats values")
+       subtitle = "Stats values from Pokemon Games (Not Pokemon Cards or Pokemon GO",
+       caption = "Source: https://www.kaggle.com/abcsds/pokemon")+
+  theme(legend.position = 'bottom', 
+        legend.background = element_blank(),
+        legend.key = element_rect(colour = NA, fill = NA),
+        legend.title = element_text(color = "white", size = 11),
+        legend.text = element_text(color = "white"),
+        axis.text.x = element_text(color="white"),
+        axis.text.y = element_text(color="white"),
+        axis.title.x = element_text(color = "white"),
+        axis.title.y = element_text(color = "white"),
+        axis.line = element_line(color = "white"),
+        plot.background = element_rect(fill = "#424242"),
+        panel.background = element_rect(fill = "#424242"),
+        panel.grid.major = element_line(color='#848484'),
+        panel.grid.minor = element_blank(),
+        plot.title = element_text(color = "white", size = 12, face = "bold"),
+        plot.subtitle = element_text(color = "white"),
+        plot.caption = element_text(color = "white", face = "italic"))
 
 # Clear environment
 rm(list = ls()) 
